d00b05b03257c8730fa2f8da57965bdba5a85136
adnan80-hub <adnanzahm05@gmail.com>
2026-03-14 22:17:16 +0000

    changing code

--- testing-code.js
+++ testing-code.js
@@ -1,5 +1,6 @@
 
 
+// ========= these codes are only for testing ======
 let textArea = document.querySelector("textarea");
 let button = document.querySelector("button");
 let inputFile = document.querySelector("input");
@@ -16,27 +17,27 @@ button.onclick = function () {
     link.click();
 }
 
-function first_non_repeating_letter(word) {
-    let convert = word.toLowerCase();
-    return convert.split("").find((ele, ind) => convert.indexOf(convert[ind]) === convert.lastIndexOf(convert[ind]))
-}
+// function first_non_repeating_letter(word) {
+//     let convert = word.toLowerCase();
+//     return convert.split("").find((ele, ind) => convert.indexOf(convert[ind]) === convert.lastIndexOf(convert[ind]))
+// }
 
-console.log(first_non_repeating_letter("stress")) // t
-console.log(first_non_repeating_letter("sTreSS")) // T
-console.log(first_non_repeating_letter("sssssss")) // ""    
+// console.log(first_non_repeating_letter("stress")) // t
+// console.log(first_non_repeating_letter("sTreSS")) // T
+// console.log(first_non_repeating_letter("sssssss")) // ""
 
-console.log("=".repeat(33));
+// console.log("=".repeat(33));
 
 
-function formatName(theName) {
-    let convert = theName.toLowerCase();
-    let result = convert.trim().split(/\s+/).map((ele) => ele[0]).join(".")
-    return result[0].toUpperCase() + result.slice(1).toLowerCase()
-}
+// function formatName(theName) {
+//     let convert = theName.toLowerCase();
+//     let result = convert.trim().split(/\s+/).map((ele) => ele[0]).join(".")
+//     return result[0].toUpperCase() + result.slice(1).toLowerCase()
+// }
 
-console.log(formatName("  Osama   Elzero  ")); // O.e
-console.log(formatName("Osama Elzero")); // O.e
-console.log(formatName("Elzero Web School")); // E.w.s
+// console.log(formatName("  Osama   Elzero  ")); // O.e
+// console.log(formatName("Osama Elzero")); // O.e
+// console.log(formatName("Elzero Web School")); // E.w.s
 
-console.log("=".repeat(33));
+// console.log("=".repeat(33));
 
